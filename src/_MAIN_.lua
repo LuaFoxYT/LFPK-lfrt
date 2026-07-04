49dca8ffb4f0298e199f62ee6f0afe52e15df7fe
luafoxyt <theluafox86yt@gmail.com>
2026-07-04 15:19:17 -0400

    Update.. Thats it

--- src/_MAIN_.lua
+++ src/_MAIN_.lua
@@ -4,30 +4,28 @@ if not ok then
 	print("luacurl needs to be installed to install it type\nluarocks install lua-curl")
 end
 local c = cURL.easy_init()
-
-c:setopt_url("https://luafoxyt.github.io/lfpk-lfrt/repos.txt")
-local dat = ""
-c:setopt_writefunction(function(_, data)
-dat = dat .. data
-end, "write") -- Writes chunks directly to file disk
-local success, err = pcall(function() c:perform() end)
-c:close()
-if success then print("Repos Download Complete!") else print("Error:", err) end
-local repos = table.parse(dat)
 local args = {...}
-if args[1] == 'install' then
-	for i, v in ipairs(repos) do
-		local url = v:gsub('@ONE@', args[2])
-		local c = cURL.easy_init()
-
-c:setopt_url("https://luafoxyt.github.io/lfpk-lfrt/repos.txt")
-local dat = ""
+local lfpk = class:getAPI('lfpk', 0)
+local function ins(a, b, c)
+c:setopt_url("https:/raw.githubusercontent.com/" .. a .. '/refs/heads/' .. (b or "master") .. "/lfpk/lfrt/" .. (c or "latest") .. ".lfar")
+local fn = "./.tmp" .. math.random(99999) .. ".lfpk"
+	local f = fs.open(fn, 'wb')
 c:setopt_writefunction(function(_, data)
-dat = dat .. data
+f:write(data)
 end, "write") -- Writes chunks directly to file disk
 local success, err = pcall(function() c:perform() end)
 c:close()
-if success then print("Installer Script Download Complete!") else print("Error:", err) end
-load(dat, '=dls', 't', _ENV)()
+f:flush()
+f:close()
+if success then
+	print("file downloaded now installing")
+	local pk = lfpk:parse(fn)
+	for name, info in pk.info.Deps do
+			ins(name, info.verion, info.branch)
+	end
+	lfpk:install(pk)
+else
+	print("Error:", err)
 end
 end
+ins(args[2], args[3], args[4])
